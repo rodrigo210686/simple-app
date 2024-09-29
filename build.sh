0dec120d825474f2ef730e0066aeee4afd7b9c82
Rodrigo Barbosa <59710101+rodrigo210686@users.noreply.github.com>
2024-09-29 09:22:43 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /root/simple-app
+docker build -t simple-web-app .
+ECR_REPO=$(aws ecr describe-repositories --region us-east-1 --repository-names survey-app-repo --query 'repositories[*].repositoryUri' --output text | cut -d'/' -f1)
+ECR_PATH=$(aws ecr describe-repositories --region us-east-1 --repository-names survey-app-repo --query 'repositories[*].[repositoryUri]' --output text)
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REPO
+docker tag simple-web-app:latest $ECR_PATH:latest
+docker push $ECR_PATH:latest
